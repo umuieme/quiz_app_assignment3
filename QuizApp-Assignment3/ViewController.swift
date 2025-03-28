9560df6cb85688e1bf4128f4bd9631b7e4d5849a
Umesh Basnet <basnet0umesh@gmail.com>
2025-03-28 09:53:21 -0400

    - [x] fix issue with navigation bar hidden after hiding in play quiz screen - [x] disable play quiz when no question added

--- QuizApp-Assignment3/ViewController.swift
+++ QuizApp-Assignment3/ViewController.swift
@@ -9,9 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnStartQuiz: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(false, animated: false)
+        if(QuizManager.shared.getQuestionList().isEmpty){
+            btnStartQuiz.isEnabled = false
+            btnStartQuiz.setTitle("Quiz is empty", for: .normal)
+        } else {
+            btnStartQuiz.isEnabled = true
+            btnStartQuiz.setTitle("Start Quiz", for: .normal)
+
+        }
     }
 
 
